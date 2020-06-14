a9956d1546bcdccba3d6b387d8262d0836c3fe58
Jason Chen <iam31337@hotmail.com>
2020-06-14 10:38:07 -0700

    update migrations (note: for your local db, without mssql (we are using sqlite right now), you can delete all these migrations and do 'add-migration initsqlite' and 'update-database' command in Package Manager Console

--- Hippra/Migrations/20200614172551_updateCaseComments.Designer.cs
+++ Hippra/Migrations/20200614172551_updateCaseComments.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Hippra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hippra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200614172551_updateCaseComments")]
+    partial class updateCaseComments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Hippra/Migrations/20200614172551_updateCaseComments.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hippra.Migrations
+{
+    public partial class updateCaseComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "imgUrl",
+                table: "CaseComments",
+                newName: "ImgUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImgUrl",
+                table: "CaseComments",
+                newName: "imgUrl");
+        }
+    }
+}
